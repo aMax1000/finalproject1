633e6ed02a2e20adf96871ad76194835948d1706
aMax1000 <mail2amax@gmail.com>
2026-06-15 23:57:08 +0300

    Update sandfall.h

--- sandfall.h
+++ sandfall.h
@@ -5,23 +5,21 @@
 #include "safepixel1,2,5.h"
 
 
-template <int Size, int Funcsize>
-void gateopen(Pixel*** arr, int col, int row,
-    pairset& pairupdateset,
-    funcexunit<Size, Funcsize>& funcs, vector<updatee>& changeset, updatesets whatupdetesetuse) {
-    for (pair<int, int> n : pairupdateset.os[whatupdetesetuse]) {
-        if (funcs.cond1(arr[n.first][n.second])) {
-            swap(arr[n.first][n.second],gatearr[n.first][n.second]);
+void gateopen(Pixel*** arr, int col, int row, pairset& pairupdateset) {
+    for (pair<int, int> n : pairupdateset.os[CLOSEGATE]) {
+        if (arr[n.first][n.second]->type==GATE) {
+            delete arr[n.first][n.second];
+            arr[n.first][n.second] = new Void;
         }
     }
 }
-template <int Size, int Funcsize>
-void gateclose(Pixel*** arr, int col, int row,
-    pairset& pairupdateset,
-    funcexunit<Size, Funcsize>& funcs, vector<updatee>& changeset, updatesets whatupdetesetuse) {
-    for (pair<int, int> n : pairupdateset.os[whatupdetesetuse]) {
-        if (funcs.cond1(arr[n.first][n.second])) {
-            arr[n.first][n.second]=new Counductor;
-        }
-    }
-}
+//template <int Size, int Funcsize>
+//void gateclose(Pixel*** arr, int col, int row,
+//    pairset& pairupdateset,
+//    funcexunit<Size, Funcsize>& funcs, vector<updatee>& changeset, updatesets whatupdetesetuse) {
+//    for (pair<int, int> n : pairupdateset.os[whatupdetesetuse]) {
+//        if (funcs.cond1(arr[n.first][n.second])) {
+//            arr[n.first][n.second]=new Counductor;
+//        }
+//    }
+//}
