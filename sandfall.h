82cb1a767a1e341c8732878f1fdd15ba480df694
aMax1000 <mail2amax@gmail.com>
2026-06-16 17:57:00 +0300

    Update sandfall.h

--- sandfall.h
+++ sandfall.h
@@ -3,6 +3,7 @@
 #include "basicslib.h"
 #include "constdata.h"
 #include "safepixel1,2,5.h"
+#include "BMPreader.h"
 
 
 void gateopen(Pixel*** arr, int col, int row, pairset& pairupdateset, signed char** gatearr) {
@@ -29,5 +30,51 @@ void gateclose(Pixel*** arr, int col, int row, pairset& pairupdateset, signed ch
 void gateupdate(int col, int row, pairset& pairupdateset, signed char** gatearr) {
     for (pair<int, int> n : pairupdateset.os[GATEUPDATE]) {
        gatearr[n.first][n.second] -= 2;
+    }
+}
+
+
+
+
+Pixel* pixeltopixel(Pixelbmp a, Pixel* b) {
+        switch ((a.b) + (a.g) << 8 + (a.r) << 16)
+        {
+        case (0x):
+            delete b;
+            b = new BasicMat;
+            b->type = SAND;
+            break;
+        case IRON:
+            break;
+        case COPPER:
+            break;
+        case BEDROCK:
+            break;
+        case WATER:
+            break;
+        case MOLTEN_IRON:
+            break;
+        case MOLTEN_COPPER:
+            break;
+        case GATE:
+            break;
+        default:
+            break;
+        }
+    //switch ()
+    //{
+    //    case 
+    //default:
+    //    break;
+    //}
+    return b;
+}
+
+void pictoarr(vector<vector<Pixelbmp>> pic, Pixel*** arr, int COL, int ROW) {
+    if ((pic.size() != COL) or pic[0].size() != ROW) cout << "PIC AND BOARD NOT THE SAME SIZE";
+    for (int i = 0; i < COL; i++) {
+        for (int j = 0; j < ROW; j++) {
+          pixeltopixel(pic[i][j], arr[i][j]);
+        }
     }
 }
