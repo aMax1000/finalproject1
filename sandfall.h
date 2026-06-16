9146d6e8ce9cdf9f2675e4ff5a06232df3f823c1
aMax1000 <mail2amax@gmail.com>
2026-06-16 15:16:30 +0300

    Update sandfall.h

--- sandfall.h
+++ sandfall.h
@@ -5,21 +5,29 @@
 #include "safepixel1,2,5.h"
 
 
-void gateopen(Pixel*** arr, int col, int row, pairset& pairupdateset) {
+void gateopen(Pixel*** arr, int col, int row, pairset& pairupdateset, signed char** gatearr) {
     for (pair<int, int> n : pairupdateset.os[CLOSEGATE]) {
-        if (arr[n.first][n.second]->type==GATE) {
+        if (arr[n.first][n.second]->type == GATE and arr[n.first][n.second]->getbit(POWER) and gatearr[n.first][n.second] == 0) {
             delete arr[n.first][n.second];
             arr[n.first][n.second] = new Void;
+            gatearr[n.first][n.second] = 3;
+            pairupdateset.ns[GATEUPDATE].push_back(n);
         }
     }
 }
-//template <int Size, int Funcsize>
-//void gateclose(Pixel*** arr, int col, int row,
-//    pairset& pairupdateset,
-//    funcexunit<Size, Funcsize>& funcs, vector<updatee>& changeset, updatesets whatupdetesetuse) {
-//    for (pair<int, int> n : pairupdateset.os[whatupdetesetuse]) {
-//        if (funcs.cond1(arr[n.first][n.second])) {
-//            arr[n.first][n.second]=new Counductor;
-//        }
-//    }
-//}
+void gateclose(Pixel*** arr, int col, int row, pairset& pairupdateset, signed char** gatearr) {
+    for (pair<int, int> n : pairupdateset.ns[CLOSEGATE].arr) {
+        if (gatearr[n.first][n.second] == 1) {
+            delete arr[n.first][n.second];
+            arr[n.first][n.second] = new Counuctor;
+            arr[n.first][n.second]->type=GATE;
+            gatearr[n.first][n.second] = 2;
+            pairupdateset.ns[GATEUPDATE].push_back(n);
+        }
+    }
+}
+void gateupdate(int col, int row, pairset& pairupdateset, signed char** gatearr) {
+    for (pair<int, int> n : pairupdateset.os[GATEUPDATE]) {
+       gatearr[n.first][n.second] -= 2;
+    }
+}
